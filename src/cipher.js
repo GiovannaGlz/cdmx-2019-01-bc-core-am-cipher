bcb5cc39b98ce1f7fd64b74c9fec45b0b299e3b3
giovanna08 <priscila_glz_27@hotmail.es>
2019-01-18 10:16:35 -0600

    Subiendo segunda operación, decifrado

--- src/cipher.js
+++ src/cipher.js
@@ -1,22 +1,27 @@
 window.cipher = {
- encode: (desplazamiento, mensajeObtenido) => {
+ encode: (desplazamiento, mensajeObtenido) => { //función para codificar dentro del objeto window
+    let resultado = '';
+    for (let i=0; i < mensajeObtenido.length; i++){
+        let ascii = mensajeObtenido[i].charCodeAt() //variable que almacena el mensaje en código ascii
+        let nuevoString = (ascii-65+parseInt(desplazamiento))%26+65;//variable que almacena el codigo ascii con el nuevo posicionamiento
+        let stringCharCodeAt = String.fromCharCode (nuevoString); //variable que transforma el nuevo codigo ascii a otro string
+        resultado=  resultado + stringCharCodeAt;//se almacena el resultado de for en cada iteración  
+    };
+    console.log(resultado);
+    return resultado; //devuelve el resultado final
+              
+ },
+
+ decode: (desplazamiento, mensajeObtenido) => {
     let resultado = '';
     for (let i=0; i < mensajeObtenido.length; i++){
         let ascii = mensajeObtenido[i].charCodeAt()
-        let nuevoString = (ascii-65+parseInt(desplazamiento))%26+65;
-        let stringCharCodeAt = String.fromCharCode (nuevoString);
-        resultado=  resultado + stringCharCodeAt;  
+        let nuevoString = (ascii-65-parseInt(desplazamiento))%26+65;
+        //console.log (nuevoString);
+        let stringCharCodeAt = String.fromCharCode(nuevoString);
+        resultado=  resultado + stringCharCodeAt;
     };
     console.log(resultado);
-    
-    // let stringCodeAtt = String.charCodeAtt (var);
-    // console.log (stringCodeAtt);
-    // let nuevoString = (stringCodeAtt - 65 + offset)%26+65;
-    
- },
-
- decode:
- function decode (string, offset) {
-
+    return resultado;
+    },
  }
-} 
