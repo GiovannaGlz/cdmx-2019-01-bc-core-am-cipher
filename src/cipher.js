b97d1664661b91393854fda44a02198d99536e39
giovanna08 <priscila_glz_27@hotmail.es>
2019-01-23 12:25:37 -0600

    Hacker edition, cifrar y decifrar minúsculas y mayúsculas, respetando espacios y signos especiales

--- src/cipher.js
+++ src/cipher.js
@@ -1,27 +1,53 @@
 window.cipher = {
- encode: (desplazamiento, mensajeObtenido) => { //función para codificar dentro del objeto window
+ encode: (desplazamiento, mensajeObtenido) => { 
+     //función para codificar dentro del objeto window
     let resultado = '';
-    for (let i=0; i < mensajeObtenido.length; i++){
-        let ascii = mensajeObtenido[i].charCodeAt() //variable que almacena el mensaje en código ascii
-        let nuevoString = (ascii-65+parseInt(desplazamiento))%26+65;//variable que almacena el codigo ascii con el nuevo posicionamiento
-        let stringCharCodeAt = String.fromCharCode (nuevoString); //variable que transforma el nuevo codigo ascii a otro string
-        resultado=  resultado + stringCharCodeAt;//se almacena el resultado de for en cada iteración  
+    for (let i=0; i < mensajeObtenido.length; i++){           
+        let ascii = mensajeObtenido[i].charCodeAt() 
+        //variable que almacena el mensaje en código ascii              
+        
+        if (ascii >=65 && ascii <= 90){
+            let nuevoString = (ascii-65+parseInt(desplazamiento))%26+65;
+            //variable que almacena el codigo ascii con el nuevo posicionamiento
+            let stringCharCodeAt = String.fromCharCode (nuevoString); 
+            //variable que transforma el nuevo codigo ascii a otro string
+            resultado=  resultado + stringCharCodeAt;
+            //se almacena el resultado de for en cada iteración  
+        } else if (ascii >=97 && ascii <= 122) {
+            //condicional para minúsculas
+            let nuevoString = (ascii-97+parseInt(desplazamiento))%26+97;
+            let stringCharCodeAt = String.fromCharCode (nuevoString); 
+            resultado=  resultado + stringCharCodeAt;
+        } else if ((ascii >=32 && ascii <= 64) || (ascii >= 123 && ascii <= 254)) {
+            //condicional para caracteres especiales y espacios
+            let stringCharCodeAt = String.fromCharCode (ascii); 
+            resultado=  resultado + stringCharCodeAt;
+        }   
     };
-    console.log(resultado);
-    return resultado; //devuelve el resultado final
-              
+    //console.log(resultado);
+    return resultado;         
  },
 
  decode: (desplazamiento, mensajeObtenido) => {
     let resultado = '';
     for (let i=0; i < mensajeObtenido.length; i++){
         let ascii = mensajeObtenido[i].charCodeAt()
-        let nuevoString = (ascii-65-parseInt(desplazamiento))%26+65;
-        //console.log (nuevoString);
-        let stringCharCodeAt = String.fromCharCode(nuevoString);
-        resultado=  resultado + stringCharCodeAt;
+        
+        if (ascii >=65 && ascii <= 90){
+            let nuevoString = (ascii+65-parseInt(desplazamiento))%26+65;
+            let stringCharCodeAt = String.fromCharCode (nuevoString);
+            resultado=  resultado + stringCharCodeAt;
+        } else if (ascii >=97 && ascii <= 122) {
+            let nuevoString = (ascii-122-parseInt(desplazamiento))%26+122;
+            let stringCharCodeAt = String.fromCharCode (nuevoString); 
+            resultado=  resultado + stringCharCodeAt;
+        } else if ((ascii >=32 && ascii <= 64) || (ascii >= 123 && ascii <= 254)) { 
+            let stringCharCodeAt = String.fromCharCode (ascii); 
+            resultado=  resultado + stringCharCodeAt;
+        }
+        
     };
-    console.log(resultado);
+    //console.log(resultado);
     return resultado;
     },
  }
--- src/index.js
+++ src/index.js
@@ -5,15 +5,15 @@ const botonCifrar = document.getElementById('opcionEncode');
 const botonDecifrar = document.getElementById ('opcionDecode');
 
 botonCifrar.addEventListener("click",()=>{
-    let mensajeObtenido = string.value.toUpperCase();
+    let mensajeObtenido = string.value;
     let desplazamiento = offset.value;
-    resultadoCifrado = window.cipher.encode(desplazamiento,mensajeObtenido);
+    let resultadoCifrado = window.cipher.encode(desplazamiento,mensajeObtenido);
     document.getElementById('nuevoMensaje').innerHTML=resultadoCifrado;    
     })
 
 botonDecifrar.addEventListener("click",()=>{
-    let mensajeObtenido = string.value.toUpperCase();
+    let mensajeObtenido = string.value;
     let desplazamiento = offset.value;
-    resultadoDecodificado = window.cipher.decode(desplazamiento,mensajeObtenido);
+    let resultadoDecodificado = window.cipher.decode(desplazamiento,mensajeObtenido);
     document.getElementById('nuevoMensaje').innerHTML=resultadoDecodificado;    
     })
